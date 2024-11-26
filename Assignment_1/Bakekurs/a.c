a930fb7bb345ef3ee0c92cf20968bb51498d3f14
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2024-11-26 17:12:13 +0100

    make code more readable

--- Assignment_1/Bakekurs/a.c
+++ Assignment_1/Bakekurs/a.c
@@ -10,13 +10,13 @@ int main() {
   int converted = atoi(inString);
 
   char* ingredientName[] = {"sukker (g)", "smør (g)", "sjokolade (g)", "egg", "hvetemel (g)"};
-  int ingredientArraySize = *(&ingredientName + 1) - ingredientName;
+  int ingredientArraySize = sizeof(ingredientName) / sizeof(ingredientName[0]);
 
   float ingredientCount[] = {400.0/48.0, 320.0/48.0, 500.0/48.0, 2.0/48.0, 460.0/48.0};
 
   for(int i = 0; i < ingredientArraySize; i++) {
     printf("%s: %.1f\n", ingredientName[i], ingredientCount[i] * converted);
   }
-  
+
   return 0;
 }
